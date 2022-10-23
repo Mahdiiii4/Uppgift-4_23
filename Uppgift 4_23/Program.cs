5f627e3459e0f79764aa9c308547b707f011c532
Mahdiiii4 <mhdisy0@gmail.com>
2022-10-23 22:42:31 +0200

    Add project files.

---
+++ Uppgift 4_23/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Uppgift4_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en ett tal");
+            int tal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ord slut om du vill avsluta eller ord fort om du vill fortsata");
+            string svar = Console.ReadLine();
+
+            if (svar == "fort")
+            {
+                Console.WriteLine("Skriv en ett tal");
+                int tal2 = int.Parse(Console.ReadLine());
+
+                while (svar == "fort")
+                {
+
+                 Console.WriteLine("Skriv in ord slut om du vill avsluta eller ord fortsat om du vill fortsata");
+                    svar = Console.ReadLine();
+
+                 if (svar == "fort")
+                 {
+                        Console.WriteLine("Skriv en ett tal");
+                            tal2 = int.Parse(Console.ReadLine());
+                 }
+                 else if(svar == "slut")
+                 {
+                     break;
+                 }
+
+                }
+                 if(tal2 > tal)
+                 {
+                     Console.WriteLine("Den storsta tall ar " + tal2 + ".");
+                 }
+                 else if (tal2 < tal)
+                 {
+                     Console.WriteLine("Den storsta tall ar " + tal + ".");
+                 }
+            }
+            else if (svar == "slut")
+            {
+               Console.WriteLine("Den storsta tall ar " + tal + ".");
+            }
+        }
+    }
+}
